2a5ad48901125994683fc5028514d5b14a0c8bda
Lipasa Mundha <110357117+LowkeyLipasa@users.noreply.github.com>
2026-05-10 22:28:42 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        //frequency map
+        if(s.length()!=t.length())
+            return false;
+        int n=s.length();
+        int freq[]=new int[26];
+        for(char c: s.toCharArray())
+            freq[c-'a']++;
+
+        for(char c: t.toCharArray())
+            freq[c-'a']--;
+
+        for(int i=0;i<26;i++)
+            if(freq[i]!=0)
+                return false;
+        
+        return true;
+
+        //hash map
+        // Map<Character,Integer> map=new HashMap<>();
+        // for(char c: s.toCharArray())
+        //     map.put(c,map.getOrDefault(c,0)+1);
+
+        // for(char c: t.toCharArray()){
+        //     if(!map.containsKey(c))
+        //         return false;
+
+        //     map.put(c,map.get(c)-1);
+
+        //     if(map.get(c)==0)
+        //         map.remove(c);
+        // }
+
+        // return (map.size()==0) ? true : false;
+        
+    }
+}
